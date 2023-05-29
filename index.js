c02d4969d9d0df5b2f528ac8cabe26fcde1eecc2
iraklitegadze <115358426+iraklitegadze@users.noreply.github.com>
2023-05-29 16:34:04 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    var x = document.getElementById("content");
+    if (x.style.display === "none"  ) {
+      x.style.display = "block";
+      y.style.display = "none";
+      
+    }  else {
+        x.style.display = "none";
+        
+    }
+}
+
+
